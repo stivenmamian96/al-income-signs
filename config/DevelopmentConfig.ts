3838fc27c4b1cc8ad78fffdec524343450359cdd
Stiven Mamián <stiven.mamian@alegra.com>
2024-12-04 20:03:08 -0500

    Feature: add development config

---
+++ config/DevelopmentConfig.ts
@@ -0,0 +1,24 @@
+import { IEnvironmentConfig } from "./interface/IEnvironmentConfig"
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+
+const pathResoved = path.resolve(process.cwd(), '.env');
+dotenv.config({ path: pathResoved });
+
+const Environment: IEnvironmentConfig['Environment'] = {
+    AWS_SIGNATURES_BUCKET_NAME: process.env.SIGNS_BUCKET,
+    AWS_SIGNATURES_DATABASE_TABLE: process.env.SIGNS_DATABASE_TABLE,
+    AWS_DEPLOYMENT_PROFILE: process.env.AWS_PROFILE,
+    AWS_LOCAL_REGION: process.env.AWS_REGION,
+    SECURITY_TOKEN: process.env.SECURITY_TOKEN,
+}
+
+export const DevelopmentConfig: IEnvironmentConfig = 
+{
+    Serverless: {
+        SIGNATURES_DATABASE_ARN: 'arn:aws:dynamodb:us-east-1:176986283156:table/alegra-income-signs',
+        SIGNATURES_BUCKET_ARN: 'arn:aws:s3:::alegrasignsimages',
+    },
+    Environment
+}
