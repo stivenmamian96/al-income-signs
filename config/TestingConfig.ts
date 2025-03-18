bdf44d2e4b64e8550a78e468ccdc22652c3d7896
Stiven Mamián <stiven.mamian@alegra.com>
2025-03-18 17:27:06 -0500

    Refactor: Add CANVAS_LAYER_ARN to environment configurations and update serverless configuration to use it

--- config/TestingConfig.ts
+++ config/TestingConfig.ts
@@ -11,7 +11,8 @@ export const TestingConfig: IEnvironmentConfig =
     Serverless: {
         SIGNATURES_DATABASE_ARN: `arn:aws:dynamodb:us-east-1:647089136475:table/${Environment.AWS_SIGNATURES_DATABASE_TABLE}`,
         SIGNATURES_BUCKET_ARN: `arn:aws:s3:::${Environment.AWS_SIGNATURES_BUCKET_NAME}`,
-        LAMBDA_AUTHORIZER_ARN: 'arn:aws:lambda:us-east-1:812368164925:function:alegra-auth-test-authorizer'
+        LAMBDA_AUTHORIZER_ARN: 'arn:aws:lambda:us-east-1:812368164925:function:alegra-auth-test-authorizer',
+        CANVAS_LAYER_ARN: 'arn:aws:lambda:us-east-1:647089136475:layer:canvas-nodejs:1'
     },
     Environment
 }
