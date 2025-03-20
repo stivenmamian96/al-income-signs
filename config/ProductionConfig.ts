d3f3296b6bce52a1dc113a30335a32563f0fe07c
Stiven Mamián <stiven.mamian@alegra.com>
2025-03-19 20:01:41 -0500

    Feature: Add CORS configuration to environment settings for Development, Production, and Testing environments

--- config/ProductionConfig.ts
+++ config/ProductionConfig.ts
@@ -12,7 +12,14 @@ export const ProductionConfig: IEnvironmentConfig =
         SIGNATURES_DATABASE_ARN: `arn:aws:dynamodb:us-east-1:104136624701:table/${Environment.AWS_SIGNATURES_DATABASE_TABLE}`,
         SIGNATURES_BUCKET_ARN: `arn:aws:s3:::${Environment.AWS_SIGNATURES_BUCKET_NAME}`,
         LAMBDA_AUTHORIZER_ARN: 'arn:aws:lambda:us-east-1:857745039400:function:alegra-auth-prod-authorizer',
-        CANVAS_LAYER_ARN: 'arn:aws:lambda:us-east-1:104136624701:layer:canvas-nodejs:1'
+        CANVAS_LAYER_ARN: 'arn:aws:lambda:us-east-1:104136624701:layer:canvas-nodejs:1',
+        CORS_CONFIG: {
+            ALLOWED_ORIGINS: ['https://app.alegra.com'],
+            ALLOWED_HEADERS: ['*'],
+            ALLOWED_METHODS: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            ALLOW_CREDENTIALS: false,
+            EXPOSED_RESPONSE_HEADERS: [],
+        }
     },
     Environment
 }
